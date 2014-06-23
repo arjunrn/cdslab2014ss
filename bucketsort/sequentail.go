37e38443002b8dd7c74dbd4795bebb0f955924a4
Arjun Naik <arjun@arjunnaik.in>
2014-06-24 00:43:13 +0200

    Parallelized the implementation of bucketsort.

--- bucketsort/sequentail.go
+++ bucketsort/sequentail.go
@@ -2,27 +2,34 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
 	"bufio"
 	"container/list"
 	"sort"
+	"strings"
+	"time"
+	"bytes"
+	"runtime"
 )
 
-const(
+const (
 	BucketNum = (0x7E - 0x21 + 1)
+	//	ReadBufferSize = 8
+	ReadBufferSize  = 1024 * 1024 * 1204 * 8
+	WriteBufferSize = 1024
 )
 
 type Bucket struct{
-	keys list.List
-	bucketArr []string
+	keys        list.List
+	bucketArr   []string
+	doneChannel chan int
 }
 
 func (b *Bucket) getLength() int {
 	return b.keys.Len()
 }
 
-func (b *Bucket) sort()  {
+func (b *Bucket) sort() {
 	b.bucketArr = make([]string, b.keys.Len())
 	i := 0
 	for e := b.keys.Front() ; e != nil ; e = e.Next() {
@@ -30,75 +37,82 @@ func (b *Bucket) sort()  {
 		i++
 	}
 	sort.Strings(b.bucketArr)
+	b.doneChannel <- 1
 }
 
-func bucketsort(inputString []string, writer *bufio.Writer) {
-	buckets := make([]Bucket, BucketNum)
+func bucketSort(buckets []Bucket) {
 
-	for i := range inputString {
-		charString := inputString[i]
-		bucketIndex := uint8(charString[0]) - 0x21
-		if bucketIndex >= BucketNum {
-			panic(fmt.Sprintf("Index out of bucket slice range: %d for index: %d", bucketIndex, i))
-		}
-		buckets[bucketIndex].keys.PushFront(charString);
 
+	for i := range buckets {
+		buckets[i].doneChannel = make(chan int)
+		//	fmt.Println("Bucket %d size: %d", i, buckets[i].getLength())
+		go buckets[i].sort()
 	}
 
 	for i := range buckets {
-		fmt.Println("Bucket %d size: %d", i, buckets[i].getLength())
-		buckets[i].sort()
+		<-buckets[i].doneChannel
 	}
 
-	for i := range buckets {
-		for j := range buckets[i].bucketArr{
-			writer.WriteString(buckets[i].bucketArr[j])
-		}
-	}
 }
 
-func main(){
+func main() {
+	runtime.GOMAXPROCS(4)
 	fi, err := os.Open("bucketsort.in")
 	if err != nil { panic(err) }
 
-	defer func(){
+	defer func() {
 		if err := fi.Close(); err != nil {
 			panic(err)
 		}
 	}()
 
 	fo, err := os.Create("bucketsort.out")
-    if err != nil { panic(err) }
-    // close fo on exit and check for its returned error
-    defer func() {
-        if err := fo.Close(); err != nil {
-            panic(err)
-        }
-    }()
-    // make a write buffer
-    writer := bufio.NewWriter(fo)
+	if err != nil { panic(err) }
+	// close fo on exit and check for its returned error
+	defer func() {
+		if err := fo.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	// make a write buffer
 
-	reader := bufio.NewReader(fi)
-	list := list.New()
-	counter := 0
-	for {
-		line, err := reader.ReadString('\n')
-		if err != nil && err != io.EOF { panic(err) }
-		if line == "" { break }
-		counter++
-		list.PushFront(line)
+
+	buckets := make([]Bucket, BucketNum)
+	rbuff := make([]byte, ReadBufferSize)
+
+	start := time.Now()
+
+	for n, e := fi.Read(rbuff) ; e == nil ; n, e = fi.Read(rbuff) {
+		readString := strings.TrimSpace(string(rbuff[:n]))
+		inputValues := strings.Split(readString, "\n")
+		for i := range inputValues {
+			charString := inputValues[i]
+			bucketIndex := uint8(charString[0]) - 0x21
+			if bucketIndex >= BucketNum {
+				panic(fmt.Sprintf("Index out of bucket slice range: %d for index: %d", bucketIndex, i))
+			}
+			buckets[bucketIndex].keys.PushFront(charString);
+		}
 	}
 
-	fmt.Println("Number of Lines: ", counter)
+	elapsedRead := time.Since(start)
+	fmt.Printf("Reading and file and bucketing took %s\n", elapsedRead)
 
-	number_slice := make([]string, list.Len())
-	fmt.Println("Length of Number Slice: ", len(number_slice))
+	start = time.Now()
+	bucketSort(buckets)
+	elapsedRead = time.Since(start)
+	fmt.Printf("Bucket Sort took %s\n", elapsedRead)
 
-	i := 0
-	for e := list.Front() ; e != nil ;  e = e.Next() {
-		//fmt.Println(i," : ", e.Value.(string))
-		number_slice[i] = e.Value.(string)
-		i++
+	start = time.Now()
+	writer := bufio.NewWriter(fo)
+	for i := range buckets {
+		stringBuilder := bytes.NewBufferString("")
+		for j := range buckets[i].bucketArr {
+			stringBuilder.WriteString(fmt.Sprintf("%s\n", buckets[i].bucketArr[j]))
+		}
+		writer.WriteString(stringBuilder.String())
+		writer.Flush()
 	}
-	bucketsort(number_slice, writer)
+	elapsedRead = time.Since(start)
+	fmt.Printf("Writing to file took %s\n", elapsedRead)
 }
